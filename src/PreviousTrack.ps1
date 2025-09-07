656b01cd71d34de809c6866d9e6ded3ad6cea980
TheGhostAle <Alessandrohome7@gmail.com>
2025-09-07 21:59:09 +0200

    Initial commit

---
+++ src/PreviousTrack.ps1
@@ -0,0 +1,16 @@
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class MediaKeys {
+    [DllImport("user32.dll", SetLastError = true)]
+    public static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+}
+"@
+
+$VK_MEDIA_PREV_TRACK = 0xB1
+$KEYEVENTF_EXTENDEDKEY = 0x1
+$KEYEVENTF_KEYUP = 0x2
+
+[MediaKeys]::keybd_event($VK_MEDIA_PREV_TRACK, 0, $KEYEVENTF_EXTENDEDKEY, [UIntPtr]::Zero)
+[MediaKeys]::keybd_event($VK_MEDIA_PREV_TRACK, 0, ($KEYEVENTF_EXTENDEDKEY -bor $KEYEVENTF_KEYUP), [UIntPtr]::Zero)
